05ad67e9ea1932198af18dfd7d73a9d7bb4fff02
unknown <abisovfarid14@gmail.com>
2024-03-09 15:06:58 +0400

    Es6 Inheritance

---
+++ JavaScript/ES6Prototype/ES6Inheritance/app.js
@@ -0,0 +1,23 @@
+//Es6 Inheritance
+// function Person(name,age){
+//     this.name=name;
+//     this.age=age;
+//   }
+//   Person.prototype.showinfos=function(){
+//       console.log("Ad "+this.name+"Yaş "+this.age);
+//   }
+//   // const person=new Person("Farid",21);
+//   // console.log(person)
+  
+//   function Employee(name,age,salary){ 
+//      this.name=name;
+//      this.age=age;
+//      this.salary=salary;
+//   }
+//   Employee.prototype=Object.create(Person.prototype);
+//   const emp1= new Employee("Nurlan ",21,1000);
+//   console.log(emp1)
+//   emp1.showinfos() //Person metodun miras alaraq istifade edildi.
+
+
+////Es6 Inheritance
--- JavaScript/ES6Prototype/StaticMethods/app.js
+++ JavaScript/ES6Prototype/StaticMethods/app.js
@@ -1,10 +1,18 @@
-//Static method
+//Static method-Object yaratmadan bu metodan istifade etmek
 
 class Mathematic{
-    cube(x){
+    sqrt(x){
+        console.log(x*x)
+    }
+    static cube(x){
         console.log(x*x*x);
     }
 }
+
+Mathematic.cube(2) //Object yaratmadan biz cube metodundan istifade edirik
+// Mathematic.sqrt(2);
+//Static method Class'in prototype'na yazilmir ona gore de
 const math=new Mathematic();
-math.cube(2)
-console.log(math)
+console.log(math)   
+// math.cube(3); //error
+math.sqrt(2); // 4
