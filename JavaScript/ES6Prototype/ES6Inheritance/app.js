c597d86028c68a0ad76c35237b275c031f782a7e
unknown <abisovfarid14@gmail.com>
2024-03-09 16:32:18 +0400

    add

--- JavaScript/ES6Prototype/ES6Inheritance/app.js
+++ JavaScript/ES6Prototype/ES6Inheritance/app.js
@@ -20,4 +20,30 @@
 //   emp1.showinfos() //Person metodun miras alaraq istifade edildi.
 
 
+
 ////Es6 Inheritance
+class Person{  //Superclass,BaseClass
+    constructor(name,age){
+      this.name=name;
+      this.age=age;
+    }
+    showinfos(){
+        console.log("Ad "+this.name+" Yaş "+this.age);
+    }
+}
+
+class Employee extends Person { //DerivedClass,SubClass
+    constructor(name,age,salary){
+        // this.name=name;
+        // this.age=age;
+        super(name,age);
+        this.salary=salary; 
+      }
+      showinfos(){
+        console.log("Ad "+this.name+" Yaş "+this.age+" Maas "+this.salary);
+    } //overriting etdik ve bu metod istifade oluncaq +salary yazilan
+}
+
+const emp=new Employee("Farid",21,1000);
+// console.log(emp)
+emp.showinfos()
